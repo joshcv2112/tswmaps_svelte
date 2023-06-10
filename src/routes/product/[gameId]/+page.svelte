669ce99a1fc6f1605ef5c97b79eb488aedfd576c
Joshua Vaughan <joshcv2112@gmail.com>
2023-06-10 00:41:20 -0700

    setup route and loco data route files whatever

---
+++ src/routes/product/[gameId]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+import { onMount } from 'svelte';
+import { page } from '$app/stores';
+const gameId = $page.params.gameId;
+let jsonData;
+
+onMount(async () => {
+    jsonData = (await import(`../../../data/gameData/${gameId}.json`)).default;
+});
+</script>
+
+<pre>
+    ayo
+    {gameId}
+    {JSON.stringify(jsonData, null, 2)}
+</pre>
--- src/routes/product/[gameId]/[productId]/+page.svelte
+++ src/routes/product/[gameId]/[productId]/+page.svelte
@@ -6,6 +6,8 @@
     let jsonData;
     const dataPath = `../../../data/routeData/tsw2/${productId}.json`;
     
+    const route = $page.params.productId;
+    const game = $page.params.gameId;
 
     onMount(async () => {
         jsonData = (await import(`../../../data/routeData/tsw2/${productId}.json`)).default;
@@ -14,14 +16,20 @@
 
 
 <pre>
-    {dataPath}
+    game: {game}
+    route: {route}
+    dataPath: {dataPath}
 </pre>
 
 <pre>
+    jsonData:
     {JSON.stringify(jsonData, null, 2)}
 </pre>
 
 <h1>Details about product {productId}</h1>
+
 <pre>
+    PAGE:
     {JSON.stringify($page, null, 2)}
 </pre>
+
